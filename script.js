1a3d3e09e468d5536e5d93b7d660d997220c8933
Sebastian <sebastianhothaza@gmail.com>
2022-10-20 23:42:14 -0400

    Fixed issues with equal button logic

--- script.js
+++ script.js
@@ -11,7 +11,10 @@ let b="";
 let op="";
 let canSelectOp = false; // Toggles when user can select an operand
 let selectedOp = false; // True once we want to start building b
+let escaped = false; // Used to define fresh a when digit selected after using equals
 let display = document.querySelector('.display');
+
+
 
 // Adding a click listener for all buttons
 const allBtns = document.querySelectorAll('button');
@@ -31,28 +34,41 @@ function handleInput(btn){
         // Numeric input, can be clicked at any point
 
         // If number corresponds to a
-        if (!selectedOp){
+        if (!selectedOp){ 
+            if (escaped) a=""; // Reset a to fresh since coming off of equals
             a = ("" + a + mapBtn(btn));
             canSelectOp = true;
-            display.textContent = a;
+            display.textContent = a; 
+            escaped=false;
         } else { // Number received corresponds to b.
             b = ("" + b + mapBtn(btn));
             display.textContent = b;
         }
+
+
+
     } else if (canSelectOp && (btn === "btnAdd" || btn === "btnSub" || btn === "btnMul" || btn === "btnDiv")){
         op = btn;
         selectedOp = true;
+        display.textContent = mapBtn(op);
 
-        //  We ALSO perform calculation if b exists
-        if (b){
-            a = compute(a,b,op);
-            display.textContent = mapBtn(op);
+        // rolling calculation
+        if (b){ 
+            a = compute(a,b,op); // Compute new a value, b is still to be input
             b = "";
-        } else {
-            display.textContent = mapBtn(op);
-        }
+        } 
+
+
+
     } else if (btn === "btnEqu" && b){
-        display.textContent = compute(a,b,op);
+        a = compute(a,b,op);
+        b="";
+        display.textContent = a;
+        selectedOp = false; // Roll forward as a new op
+        escaped = true;
+
+
+        
     } else if (btn === "btnClr"){
         resetCalculator();
     } else if (btn === "btnDel"){
@@ -74,16 +90,15 @@ function handleInput(btn){
                 b = b.slice(0,b.length-1);
                 display.textContent = b;
             }
-        } else{ //TODO case when <0
-            if (curDisp<10){
+        } else{ 
+            if (curDisp<10){ //not working properly
                 console.log("CalcReset");
                 resetCalculator();
             } else {
                 a = curDisp.slice(0,curDisp.length-1);
+                display.textContent = a; 
                 b="";
-                display.textContent = a;   
-            }
-                    
+            }     
         }
     } else if (btn === 'btnDec'){
         //TODO
@@ -117,8 +132,16 @@ function mapBtn(btn){
 // Returns a op b where op is the id of the opBtn AS STRING!
 function compute(a, b, op){
     // We assure conversion to int for now
-    a = parseInt(a);
-    b = parseInt(b);
+    if (a == ""){
+        a = 0;
+    }else{
+        a = parseInt(a);
+    }
+    if (b == ""){
+        b = 0;
+    }else{
+        b = parseInt(b);
+    }
     switch(op){
         case "btnAdd":
             return String(a + b);
